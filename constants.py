f350282ba559efd28d838640f7f694b5d88a1016
Vladimir Novakovic <vldnovak555@gmail.com>
2020-04-25 19:55:33 +0200

    optimisation of model and added plot

--- constants.py
+++ constants.py
@@ -5,9 +5,9 @@ TEST_SET_IMAGES = 'emnist-byclass/test-images-idx3-ubyte.gz'
 TEST_SET_LABELS = 'emnist-byclass/test-labels-idx1-ubyte.gz'
 
 IMAGE_SIZE = 28
-TRAIN_DATA = 100000
+TRAIN_DATA = 690000
 #697932
-TEST_DATA = 20000
+TEST_DATA = 110000
 #116323
 
 LABEL_ENCODING = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
@@ -15,7 +15,7 @@ LABEL_ENCODING = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F
 def decode(position):
     return LABEL_ENCODING[position]
 
-EPOCHS = 12
+EPOCHS = 6
 BATCH_SIZE = 128
 CLASS_NUMBER = 62
 TEST_PROCENT = 0.2
--- model.py
+++ model.py
@@ -9,45 +9,23 @@ Created on Thu Apr 16 19:27:54 2020
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
-import constants as C
-
-
-classifier = Sequential()
-
-classifier.add(Conv2D(32, (3, 3), input_shape = (28, 28, 1), activation = 'relu'))
-
-classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
-
-classifier.add(Conv2D(64, (3, 3), activation = 'relu'))
-
-classifier.add(MaxPooling2D(pool_size = (2, 2)))
-
-classifier.add(Dropout(0.25))
-
-classifier.add(Flatten())
-
-classifier.add(Dense(512, activation = 'relu'))
-classifier.add(Dropout(0.25))
-classifier.add(Dense(144, activation = 'relu'))
-classifier.add(Dropout(0.25))
-classifier.add(Dense(C.CLASS_NUMBER, activation = 'sigmoid'))
-
-classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+from keras.optimizers import SGD
+import constants as const
 
 model = Sequential()
 
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=(28,28,1)))
+model.add(Conv2D(32, kernel_size=(3, 3), strides=(2, 2), activation='relu', input_shape=(28,28,1)))
+model.add(MaxPooling2D(pool_size=(2, 2),strides=(2, 2)))
 model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(Conv2D(128, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(512, activation='relu'))
-model.add(Dense(144, activation = 'relu'))
-model.add(Dense(144, activation = 'relu'))
-model.add(Dropout(0.25))
-model.add(Dense(62, activation='sigmoid'))
 
-model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
+model.add(Flatten())
+
+model.add(Dense(2048, activation='relu'))
+model.add(Dropout(0.25))
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.25))
+model.add(Dense(const.CLASS_NUMBER, activation='softmax'))
+
+model.compile(optimizer =  SGD(lr=0.01,momentum=0.9), loss = 'categorical_crossentropy', metrics = ['accuracy'])
